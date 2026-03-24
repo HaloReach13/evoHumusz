4d9d8ad62fddf07e08520c4d90244b66360da9fe
HaloReach13 <juhaszbazsi13@gmail.com>
2026-03-24 22:18:33 +0100

    Arrangement of cards
    Arrangement of cards

--- evohumusz2.0/src/components/CardContent.jsx
+++ evohumusz2.0/src/components/CardContent.jsx
@@ -1,45 +1,23 @@
 import React, { Component } from 'react';
-import { Box, Button, Chip, Typography } from '@mui/material';
+import '../css/NewsAndBlogs.css';
 
 export class CardContent extends Component {
     render() {
         const { model } = this.props;
         return (
             <>
-                <Box className="card-overlay" />
-
-                <Chip
-                    label={model.type.toUpperCase()}
-                    className="card-badge"
-                />
-
-                <Box className="card-content">
-                    <Typography
-                        className="card-title"
-                    >
-                        {model.title}
-                    </Typography>
-
-                    <Typography
-                        className="card-desc"
-                    >
-                        {model.description}
-                    </Typography>
-
-                    <Typography
-                        variant="overline"
-                        className="card-date"
-                    >
+                <div className="card-overlay"></div>
+                <div className="card-badge">
+                    {model.type.toUpperCase()}
+                </div>
+                <div className="card-content">
+                    <h1 className="card-title">{model.title}</h1>
+                    <p className="card-desc">{model.description}</p>
+                    <p className="card-date">
                         {new Date(model.date).toLocaleDateString('hu-HU')}
-                    </Typography>
-
-                    <button className="card-button">
-                        Olvasd el
-                    </button>
-                </Box>
-
-
-
+                    </p>
+                    <button className="card-button">Olvasd el</button>
+                </div>
             </>
         );
     }
